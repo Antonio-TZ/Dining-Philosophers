c76a721e189494e985555b252f3ba2b8711e17d9
Antonio Theodorides <theo@danskebank.dk>
2019-02-25 06:53:53 +0100

    feature: chopstick locks when picked up

---
+++ DiningPhilosophers_n/Chopstick.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading;
+
+namespace DiningPhilosophers_n {
+    internal class Chopstick {
+        public bool IsHeld { get; private set; }
+
+        internal bool Pickup() {
+            if (IsHeld) return false;
+            if (Monitor.TryEnter(this, 500)) {
+                IsHeld = true;
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ DiningPhilosophers_n/ChopstickShould.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using NUnit.Framework;
+
+namespace DiningPhilosophers_n {
+    [TestFixture]
+    public class ChopstickShould {
+
+        [Test]
+        public void lock_when_picked_up() {
+            Chopstick chopstick = new Chopstick();
+            chopstick.Pickup();
+            Assert.That(!chopstick.Pickup());
+        }
+    }
+}
