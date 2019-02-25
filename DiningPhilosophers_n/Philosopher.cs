a5107589030043a96ee892d76646d7d1afeb5835
Antonio Theodorides <theo@danskebank.dk>
2019-02-25 07:23:22 +0100

    feature: philosopher returns both chopsticks when done eating

--- DiningPhilosophers_n/Philosopher.cs
+++ DiningPhilosophers_n/Philosopher.cs
@@ -1,4 +1,36 @@
-﻿namespace DiningPhilosophers_n {
+﻿using System;
+
+namespace DiningPhilosophers_n {
     internal class Philosopher {
+        public Chopstick LeftChopstick { get; internal set; }
+        public Chopstick RightChopstick { get; internal set; }
+        internal bool HoldsBothChopsticks => LeftChopstick.IsHeld && RightChopstick.IsHeld;
+        internal bool IsNotHoldingChopsticks => !LeftChopstick.IsHeld && !RightChopstick.IsHeld;
+
+        public Philosopher() {
+            LeftChopstick = new Chopstick();
+            RightChopstick = new Chopstick();
+        }
+
+        internal bool Eat() {
+            if(HoldsBothChopsticks) {
+                ReturnBothChopsticks();
+                return true;
+            }
+            return false;
+        }
+
+        private void ReturnBothChopsticks() {
+            LeftChopstick.Putdown();
+            RightChopstick.Putdown();
+        }
+
+        internal void EatDinner() {
+            if(LeftChopstick.Pickup()) {
+                if(RightChopstick.Pickup()) {
+                    Eat();
+                }
+            }
+        }
     }
 }
--- DiningPhilosophers_n/PhilosopherShould.cs
+++ DiningPhilosophers_n/PhilosopherShould.cs
@@ -4,7 +4,24 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+using NUnit.Framework;
+
 namespace DiningPhilosophers_n {
-    class PhilosopherShould {
+    public class PhilosopherShould {
+
+        [Test]
+        public void eat_when_holding_both_chopsticks() {
+            Philosopher philosopher = new Philosopher();
+            philosopher.LeftChopstick.Pickup();
+            philosopher.RightChopstick.Pickup();
+            Assert.That(philosopher.Eat());
+        }
+
+        [Test]
+        public void return_both_chopsticks_when_done_eating() {
+            Philosopher philosopher = new Philosopher();
+            philosopher.EatDinner();
+            Assert.That(philosopher.IsNotHoldingChopsticks);
+        }
     }
 }
