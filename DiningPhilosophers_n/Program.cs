0b5cfd5eb8d8fc43d13643cfa7610d2169affa69
Antonio Theodorides <theo@danskebank.dk>
2019-02-25 06:47:07 +0100

    Add project files.

---
+++ DiningPhilosophers_n/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiningPhilosophers_n {
+    class Program {
+        static void Main(string[] args) {
+        }
+    }
+}
